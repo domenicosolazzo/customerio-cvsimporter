45977e599044eb20ca2c068c4a0f5026dc9fd833
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-06-11 16:28:58 +0200

    Working version of the importer. It imports the data to Customer.io

--- importer.py
+++ importer.py
@@ -1,16 +1,13 @@
 import csv
 import sys
-from customerio import CustomerIO
-
-
-
+import customerio 
 
 csv_name = "example.csv"
-site_id = ""
-api_key = ""
+site_id = "67791824ca146a14ee53"
+api_key = "23044f477b3963e28b83"
 
 try:
-	cio = CustomerIO(site_id, api_key)
+	cio = customerio.CustomerIO(site_id, api_key)
 	csv_reader = csv.reader( open( csv_name, 'rb' ), delimiter=',', quotechar='"' )
 	csv_list = list( csv_reader )
 	if ( len( csv_list ) <= 0 ):
@@ -18,7 +15,7 @@ try:
 
 	keys = csv_list.pop(0)
 
-	print keys
+	#print keys
 	objs = [ i for i in csv_list]
 	results = []
 	for i in csv_list:
@@ -26,12 +23,16 @@ try:
 		for x in range(len(keys)):
 			data[keys[x]] = i[x]
 		results.append(data)
-	print results
-	cio.identify(id=5, email='customer@example.com', name='Bob', plan='premium')
+	#print results
+	for customer in results:
+		cio.identify(**customer)
 except IOError, e:
 	print("The input file ({0}) was missing. Error({1}): {2}".format(csv_name,e.errno, e.strerror))
 	raise e
 except csv.Error, e:
 	print("Error opening the csv file. Error: %s" % (str(e), ))
-	raise e
+	raise 
+except customerio.CustomerIOException, e:
+	print("Error using the Customer.io library. Error: %s" % (e.message, ))
+	
 	
